bfd3c81c160aa0162e57eb6268b9a855159b46c8
CheolheeLee <wks0968@gmail.com>
2021-03-10 12:25:36 +0900

    Add body, categories

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat/constraints.dart';
+import 'package:flutter_chat/screens/home/components/categories.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          child: Text(
+            "Women",
+            style: Theme.of(context)
+                .textTheme
+                .headline5
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Categories(),
+      ],
+    );
+  }
+}
